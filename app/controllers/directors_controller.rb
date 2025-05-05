8588873b13376c842ec4e690778ee566da9164af
krutikarao1997 <krutikarao@chicagobooth.edu>
2025-05-05 07:32:23 +0000

    All aspects working

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,22 @@
 class DirectorsController < ApplicationController
   def index
-    render ({ :template => "director_templates/list"})
+    render({ :template => "director_templates/list" })
+  end
+
+  def youngest
+    @director = Director.where.not({ :dob => nil }).order({ :dob => :desc }).at(0)
+    render({ :template => "director_templates/youngest" })
+  end
+
+  def eldest
+    @director = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+    render({ :template => "director_templates/eldest" })
   end
 
   def show
     the_id = params.fetch("the_id")
-    matching_records = Director.where({ :id => the_id })
-
-    @the_director = matching_records.at(0)
-    render ({ :template => "director_templates/details"})
+    @director = Director.where({ :id => the_id }).at(0)
+    render({ :template => "director_templates/details" })
   end
 end
+
