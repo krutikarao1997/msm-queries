41c55693e13defaa06247589d2b3e39550911639
krutikarao1997 <krutikarao@chicagobooth.edu>
2025-05-05 05:55:47 +0000

    filmography works

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get("/",  { :controller => "misc", :action => "homepage" })
+
   get("/directors", { :controller => "directors", :action => "index" })
+
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
+
 end
+matching_movies
